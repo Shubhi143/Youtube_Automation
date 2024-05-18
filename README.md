b6915f9708b854a88190caf648bb7fed2568770a
Shubhi143 <shubhig389@gmail.com>
2024-05-18 12:01:50 +0530

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
-# Youtube_Automation
-A simple project to automate youtube with the help of selenium java and junit where you can navigate to youtube ,search for some input for example "Selenium" and navigating to "About YouTube",verifying the results ,then Searching for Movies from the main menu selection icons.
+# YouTube Automation
 
+# Description
+The Application Under Test is YouTube.
+
+The Aim of this project is to search for Videos and Explore Sections such as Movies, Music, News etc...
+
+This YouTube Automation project consists of 6 Testcases in Total.
+
+# TestCase 01 Description
+Go to www.youtube.com.  Check the Current URL using Assert Statements
+
+# TestCase 02 Description
+Go to YouTube.com and Assert you are on the correct URL. Click on “About” at the bottom of the sidebar, and print the message on the screen.
+
+# TestCase 03 Description
+Go to the “Films” tab and in the “Top Selling” section, scroll to the extreme right. Apply a Soft Assert on whether the movie is marked “A” for Mature or not. Apply a Soft assert on whether the movie is either “Comedy” or “Animation”.
+
+# TestCase 04 Description
+Go to the “Music” tab and in the 1st section, scroll to the extreme right. Print the name of the playlist. Soft Assert on whether the number of tracks listed is less than or equal to 50.
+
+
+# Note on This YouTube Automation Project
+1. Added Selenium Wrapper Methods
+2. Added TestNG
+3. Implemented Soft Assert
+4. Implemented Apache POI
+
+## Required Software to Install:
+```
+# java version 17
+java --version
+```
+```
+# git version 2.43.0
+git --version
+```
+```
+# gradle version 8.6
+gradle --version
+```
+
+```
+# vscode 1.88.1
+git --version
+```
+## Required Dependency to run:
+```
+# WebDriverManager
+# Selenium
+# TestNG
+# poi-ooxml
+# log4j-core
+# poi
+```
+## Instructions to Run the Code:
+```
+# to build the project
+gradle build
+```
+
+```
+# to run the project
+gradle run
+```
+
+```
+# to test the project
+gradle test
+```
---
+++ app/src/test/java/demo/TestCases.java
@@ -0,0 +1,267 @@
+package demo;
+
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestCases {
+    ChromeDriver driver;
+   
+    //@BeforeMethod
+    public TestCases()
+    {
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    
+    //@AfterMethod
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    @Test
+   
+    public void testCase01() throws InterruptedException {
+      System.out.println("Start Test case: testCase01");
+      SoftAssert softassert = new SoftAssert();
+  
+      driver.get("https://www.youtube.com/");
+      Thread.sleep(2000);
+      
+      String expectedUrl = "https://www.youtube.com/";
+      String actualUrl = driver.getCurrentUrl();
+      
+      System.out.println("Expected URL: " + expectedUrl);
+      System.out.println("Actual URL: " + actualUrl);
+      
+      softassert.assertEquals(actualUrl, expectedUrl, "URL Assertion Failed: Incorrect URL");
+
+      WebElement mainmenu=driver.findElement(By.xpath("//*[@id='guide-icon']/yt-icon-shape/icon-shape/div"));
+      mainmenu.click();
+      Thread.sleep(2000);
+      WebElement aboutLink = driver.findElement(By.xpath("//*[@id='guide-links-primary']/a[1]"));
+      aboutLink.click();
+
+     // Wait for some time to ensure the page loads completely
+    try {
+      Thread.sleep(2000); // 2 seconds
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    // Print the message on the screen
+    WebElement message1 = driver.findElement(By.xpath("//p[@class='lb-font-display-3 lb-font-color-text-primary'][1]"));
+    String messageText1 = message1.getText();
+
+    WebElement message2 = driver.findElement(By.xpath("//p[@class='lb-font-display-3 lb-font-color-text-primary'][2]"));
+    String messageText2 = message2.getText();
+
+    System.out.println("Message on the screen: " + messageText1);
+    System.out.println("Message on the screen: " + messageText2);
+
+    softassert.assertAll();
+    System.out.println("end Test case: testCase01");     
+  } 
+
+
+    
+      @Test
+      public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case: testCase02");
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        SoftAssert softAssert = new SoftAssert();
+          
+          // Navigate to the "Films" tab
+          WebElement mainmenu=driver.findElement(By.xpath("//*[@id='guide-icon']/yt-icon-shape/icon-shape/div"));
+          mainmenu.click();
+         WebElement movies = driver.findElement(By.xpath("//yt-formatted-string[text()='Movies']"));
+         movies.click();
+          
+          // Wait for the page to load completely
+          Thread.sleep(2000);
+          
+          // Scroll to the extreme right in the "Top Selling" section
+          WebElement topSellingSection = driver.findElement(By.xpath("(//ytd-button-renderer[@class='style-scope yt-horizontal-list-renderer arrow'])[2]"));
+          // JavascriptExecutor js = (JavascriptExecutor) driver;
+          // js.executeScript("arguments[0].scrollLeft = arguments[0].scrollWidth", topSellingSection);
+          
+          while (topSellingSection.isDisplayed()) {
+            
+              topSellingSection.click();
+              Thread.sleep(1000);
+          }
+
+               
+          // Verify if the movie is marked as "A" for Mature
+          WebElement matureMovie = driver.findElement(By.xpath("(//p[text()='A'])[3]"));
+
+          String ratingMovieText = matureMovie.getText();
+          softAssert.assertEquals(ratingMovieText,"A");
+
+          System.out.println("verified the rating-"+ ratingMovieText);
+
+          
+          // Verify if the movie genre is either "Comedy" or "Animation"
+          WebElement movieGenre = driver.findElement(By.xpath("(//span[contains(text(),'Comedy')])[3]"));
+
+          String GenreMovieText = movieGenre.getText();
+          softAssert.assertTrue(GenreMovieText.contains("Comedy"),"Unverified Genre");
+
+          System.out.println("verified the GenreMovie-"+ GenreMovieText);
+
+    
+          softAssert.assertAll();
+          System.out.println("End Test case: testCase02");
+        }
+
+
+          @Test
+          public void testCase03() throws InterruptedException {
+          System.out.println("Start Test case: testCase03");
+
+          driver.navigate().back();
+          Thread.sleep(3000);
+          SoftAssert softAssert = new SoftAssert();
+          WebElement mainmenu=driver.findElement(By.xpath("//*[@id='guide-icon']/yt-icon-shape/icon-shape/div"));
+          mainmenu.click();
+          WebElement musicTab = driver.findElement(By.xpath("(//yt-formatted-string[text()='Music'])[1]"));
+          musicTab.click();
+          Thread.sleep(2000); // Wait for page to load
+ 
+         // Scroll to the extreme right in the first section
+         WebElement firstSection = driver.findElement(By.xpath("(//ytd-button-renderer[@class='style-scope yt-horizontal-list-renderer arrow'])[4]"));
+         Thread.sleep(2000);
+        //  JavascriptExecutor js = (JavascriptExecutor) driver;
+        //  js.executeScript("arguments[0].scrollLeft = arguments[0].scrollWidth", firstSection);
+
+        while (firstSection.isDisplayed()) {
+            
+          firstSection.click();
+          Thread.sleep(1000);
+      }
+         
+ 
+         // Get the name of the playlist
+         WebElement playlistName = driver.findElement(By.xpath("(//h3[contains(text(),'Bollywood Dance')])[1]"));
+         String playlistTitle = playlistName.getText();
+         System.out.println("Playlist Name: " + playlistTitle);
+ 
+         // Get the number of tracks listed
+         WebElement tracksList = driver.findElement(By.xpath("(//p[@id='video-count-text' and contains(text(),'50 tracks')])[4]"));
+         String trackListTxt = tracksList.getText();
+
+        
+         // Soft Assert on whether the number of tracks listed is less than or equal to 50
+         softAssert.assertEquals(trackListTxt , "50 tracks");
+ 
+         softAssert.assertAll();
+         System.out.println("End Test case: testCase03");
+    }
+    
+        @Test
+        public void testCase04() throws InterruptedException {
+        System.out.println("Start Test case: testCase04");
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+        SoftAssert softAssert = new SoftAssert();
+        double result = 0;
+        WebElement mainmenu=driver.findElement(By.xpath("//*[@id='guide-icon']/yt-icon-shape/icon-shape/div"));
+        mainmenu.click();
+        
+        WebElement newsbutton=driver.findElement(By.xpath("(//yt-formatted-string[contains(text(),'News')])[1]"));
+        newsbutton.click();
+
+        String newslistName = newsbutton.getText();
+        System.out.println("newsbutton: " + newslistName);
+        Thread.sleep(2000);
+
+        WebElement first_Title=driver.findElement(By.xpath("(//yt-formatted-string[@id='home-content-text'])[1]"));
+        Thread.sleep(2000);
+        String firstOption=  first_Title.getText();
+        System.out.println("Headline1: " + firstOption );
+
+        WebElement second_Title = driver.findElement(By.xpath("(//yt-formatted-string[@id='home-content-text'])[2]"));
+        String secondOption =  second_Title.getText();
+        System.out.println("Headline2: " + secondOption );
+
+        WebElement third_Title = driver.findElement(By.xpath("(//yt-formatted-string[@id='home-content-text'])[3]"));
+        String thirdOption = third_Title.getText();
+        System.out.println("Headline3: " + thirdOption );
+
+        Thread.sleep(2000);
+        WebElement firstLikeCountElement = driver.findElement(By.xpath("(//span[@id='vote-count-middle'])[1]"));
+        String firstLikeCountText = firstLikeCountElement.getText();
+        
+
+        // if (firstLikeCountText.contains("k")) {
+        //   result = Double.parseDouble(firstLikeCountText.replace("k", "")) * 1000;
+        //  }
+        System.out.println("Likes for the first headline: " + firstLikeCountText);
+
+        WebElement secondLikeCountElement = driver.findElement(By.xpath("(//span[@id='vote-count-middle'])[2]"));
+        String secondLikeCountText = secondLikeCountElement.getText();
+
+        // if (secondLikeCountText.contains("k")) {
+        //   result = Double.parseDouble(secondLikeCountText.replace("k", "")) * 1000;
+        //  }
+        System.out.println("Likes for the second headline: " + secondLikeCountText);
+
+      
+        WebElement thirdLikeCountElement = driver.findElement(By.xpath("(//span[@id='vote-count-middle'])[3]"));
+        String thirdLikeCountText = thirdLikeCountElement.getText();
+
+        // if (thirdLikeCountText.contains("K")) {
+        // result = Double.parseDouble(thirdLikeCountText.replace("K", "").replace(",", "")) * 1000;
+
+        // }
+
+       int likes;
+       if (thirdLikeCountText.contains("K")) {
+       double likesValue = Double.parseDouble(thirdLikeCountText.replace("K", "").replace(",", "")) * 1000;
+       likes = (int) likesValue;
+       } else {
+       likes = Integer.parseInt(thirdLikeCountText.replace(",", ""));
+       }
+
+        System.out.println("Likes for the third headline: " + thirdLikeCountText);
+        
+         int LikeCount1 = Integer.parseInt(firstLikeCountText);
+         int LikeCount2 = Integer.parseInt(secondLikeCountText);
+        // int LikeCount3 = Integer.parseInt(thirdLikeCountText);
+
+        int resultCount= LikeCount1 + LikeCount2 + likes;
+
+        System.out.println("Total like Counts : "  + resultCount);
+        softAssert.assertAll();
+
+        System.out.println("End Test case: testCase04");
+        
+      
+    }
+    
+      
+  }
